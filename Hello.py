775e81fe162e28da670749248aa11330690d407c
Pranav Sharma <ps25372@gmail.com>
2022-03-05 19:03:56 +0530

    some bugs resolved ig

--- Hello.py
+++ Hello.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+# run the app.
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
--- test_file.py
+++ test_file.py
@@ -1,4 +1,4 @@
-from Hello import hello_world
+from app import app
 with app.test_client() as c:
     response = c.get('/')
     assert response.data == b'Hello World!'
